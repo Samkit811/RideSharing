d89bf8cda036cc889c6194d2bafb69ab39772e03
samkits <samkitsalot0@gmail.com>
2025-05-01 21:49:02 +0530

    Added location class

---
+++ src/Location.java
@@ -0,0 +1,17 @@
+public class Location {
+    private final Double latitude;
+    private final Double longitude;
+
+    public Location(Double latitude, Double longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+}
