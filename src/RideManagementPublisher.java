5755df3ad84aad70ec841f1c03aa81320f6bc73a
samkits <samkitsalot0@gmail.com>
2025-05-02 10:15:18 +0530

    Update ride management publisher interface with passenger and ride method

--- src/RideManagementPublisher.java
+++ src/RideManagementPublisher.java
@@ -2,4 +2,8 @@ public interface RideManagementPublisher {
     void registerDriver(Driver driver);
     void unregisterDriver(Driver driver);
     void notifyDrivers();
+    void registerPassenger(Passenger passenger);
+    void unregisterPassenger(Passenger passenger);
+    void addRide(Ride ride);
+    void removeRide(Ride ride);
 }
