fcf6fbe3c5d8b5d8f05427ae31b762f40dd7346d
samkits <samkitsalot0@gmail.com>
2025-05-02 10:00:31 +0530

    add ride class

---
+++ src/Ride.java
@@ -0,0 +1,60 @@
+public class Ride {
+    private Location sourceLocation;
+    private Location destinationLocation;
+    private RideType rideType;
+    private Integer price;
+    private Passenger passenger;
+    private Driver driver;
+
+    public Ride(Passenger passenger, Location sourceLocation, Location destinationLocation){
+        this.passenger = passenger;
+        this.rideType = RideType.REQUESTED;
+        this.sourceLocation = sourceLocation;
+        this.destinationLocation = destinationLocation;
+        this.calculateRideFair();
+    }
+
+    public void updateSourceLocation(Location location){
+        this.sourceLocation = location;
+        this.calculateRideFair();
+    }
+
+    public void updateDestinationLocation(Location location){
+        this.destinationLocation = location;
+        this.calculateRideFair();
+    }
+
+    private void calculateRideFair(){
+        if(this.sourceLocation != null && this.destinationLocation != null){
+           Double longitudeDifference = Math.abs(this.sourceLocation.getLongitude() - this.destinationLocation.getLongitude());
+           Double latitudeDifference = Math.abs(this.sourceLocation.getLatitude() - this.destinationLocation.getLatitude());
+           if(latitudeDifference + longitudeDifference  >= 5.0){
+               this.price = 100;
+           } else {
+               this.price = 50;
+           }
+        }
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+        this.setRideType(RideType.ACCEPTED);
+    }
+
+    private void setRideType(RideType rideType) {
+         this.rideType = rideType;
+    }
+
+    public void startRide(){
+        System.out.println("Ride is started");
+        this.setRideType(RideType.IN_PROGRESS);
+    }
+
+    public void completedRide(){
+        this.setRideType(RideType.COMPLETED);
+    }
+
+    public void cancelRide(){
+        this.setRideType(RideType.CANCELLED);
+    }
+}
